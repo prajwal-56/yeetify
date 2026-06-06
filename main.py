37fb358d4699f6ddbe7e44aa1f99a9e1240fd7e8
prajwal-56 <prajwalpraveen2006@gmail.com>
2026-06-06 23:16:56 +0530

    the other initial commit

---
+++ main.py
@@ -0,0 +1,40 @@
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi import File , UploadFile
+import os
+
+app = FastAPI()
+
+app.mount("/attachments" , StaticFiles(directory="attachments"), name="attachments")
+uploaded_files_path = "./uploads/"
+
+
+@app.get("/")
+async def root():
+    return {"message" : "Hello World"}
+
+@app.get("/yeet" , response_class=HTMLResponse)
+async def root():
+    return open("index.html").read()
+
+# when uploading ...
+@app.post("/upload")
+async def upload_item(file: UploadFile = File(...)):
+    os.makedirs(uploaded_files_path , exist_ok=True)
+    file_path = os.path.join(uploaded_files_path , file.filename)
+
+    with open(file_path , "wb") as buffer:
+        buffer.write( await file.read())
+
+    return {"Status" : "you uploaded (probably)" , "file" : file.filename }
+
+# Uploaded Files
+@app.get("/uploads")
+async def get_list():
+
+    return os.listdir(uploaded_files_path)
+
+@app.post("/something")
+async def post_something():
+    return {"message" : "this does something"}
