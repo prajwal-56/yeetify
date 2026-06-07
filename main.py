632d1063aa0cee148b1053814adbfd12bbc133e2
prajwal-56 <prajwalpraveen2006@gmail.com>
2026-06-07 23:36:12 +0530

    dynamically updating the file list

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from fastapi.responses import HTMLResponse
 from fastapi.responses import FileResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi import File , UploadFile
-
+from fastapi import WebSocket
 import os
 
 app = FastAPI()
@@ -12,6 +12,20 @@ app.mount("/attachments" , StaticFiles(directory="attachments"), name="attachmen
 app.mount("/uploads" , StaticFiles(directory="uploads") , name="uploads")    # for downloading
 
 uploaded_files_path = "uploads/"
+
+# holds the list of connected devices
+connections = []
+
+
+@app.websocket("/websocket")
+async def socket_endpoint(websocket: WebSocket):
+    await websocket.accept()
+    connections.append(websocket)
+    try:
+        while True:
+            await websocket.receive_text()  # keep the connection alive
+    except:
+        connections.remove(websocket)
 
 
 @app.get("/")
@@ -22,19 +36,25 @@ async def root():
 async def root():
     return open("index.html").read()
 
+
 # when uploading ...
 @app.post("/upload")
 async def upload_item(file: UploadFile = File(...)):
+
+
     os.makedirs(uploaded_files_path , exist_ok=True)
     file_path = os.path.join(uploaded_files_path , file.filename)
 
     with open(file_path , "wb") as buffer:
         buffer.write( await file.read())
 
+    for connection in connections:
+        await connection.send_text("new_file_appeared")
+
     return {"Status" : "you uploaded (probably)" , "file" : file.filename }
 
 # Uploaded Files
-@app.get("/uploads")
+@app.get("/files")
 async def get_list():
 
     return os.listdir(uploaded_files_path)
