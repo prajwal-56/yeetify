fe9c89f94f8179b295b0968866bc9ba7aa9ac5bd
prajwal-56 <prajwalpraveen2006@gmail.com>
2026-06-08 23:59:58 +0530

    qr on toggle

--- static/qrc.js
+++ static/qrc.js
@@ -3,6 +3,13 @@ const qr_container = document.getElementById("qrcode");
 
 
 generate_qr_btn.onclick = async () => {
+
+    if (qr_container.innerHTML !== "") {
+        
+        qr_container.innerHTML = "";
+        return;
+    }
+
     const local_ip_response = await fetch('local_ip');
     const local_ip_data = await local_ip_response.json();
     const url = `http://${local_ip_data.ip}:8000/yeet`
