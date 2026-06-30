bba2b0be664472f9cb145bee66dda0bfc6426462
prajwal-56 <prajwalpraveen2006@gmail.com>
2026-06-30 22:49:59 +0530

    ATTEMPT to fix the qrcode-url bug when hosted on docker container. Not yet fixed

--- utils.py
+++ utils.py
@@ -1,7 +1,21 @@
 import socket
 
 def get_local_ip():
-    hostname = socket.gethostname()
-    local_ip = socket.gethostbyname(hostname)
-    print(local_ip)
+
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)        # opens a UDP socket
+
+    try:
+        s.connect(("8.8.8.8" , 80))     # just to set up routing 
+        local_ip = s.getsockname()[0]   # getsockname() -> returns a tuple containing the IP and PORT , [0] is the IP
+    except:
+        local_ip = "127.0.0.1"
+    finally:
+        s.close()
+
     return local_ip
+
+# OLD Approach which breaks on docker container 
+    # hostname = socket.gethostname()
+    # local_ip = socket.gethostbyname(hostname)
+    # print(local_ip)
+    # return local_ip
